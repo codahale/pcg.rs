9bc662c3bc9624c9d5fdb75356c79ade92129542
Coda Hale <coda.hale@gmail.com>
2015-03-03 17:02:38 +0000

    rebuild pages at 4c7a734

---
+++ implementors/rand/trait.Rng.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['pcg'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/rand/rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='pcg/struct.PcgRng.html' title='pcg::PcgRng'>PcgRng</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['pcg'] = {"items":[[0,"","pcg","An implementation of the [PCG](http://www.pcg-random.org) random number\ngenerator."],[3,"PcgRng","","A [PCG](http://www.pcg-random.org) random number generator."],[11,"new_unseeded","","Returns a new `PcgRng` instance which is not seeded.",0],[11,"next_u32","","",0],[11,"reseed","","Reseed a `PcgRng`. This will panic if `seed` is entirely 0.",0],[11,"from_seed","","Create a new `PcgRng`. This will panic if `seed` is entirely 0.",0]],"paths":[[3,"PcgRng"]]};
+initSearch(searchIndex);
