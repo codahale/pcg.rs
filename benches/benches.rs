1979cf1f27286f738318e98c3117f41bb91e4e1d
Coda Hale <coda.hale@gmail.com>
2015-03-03 09:23:52 -0800

    Added benchmark for fill_bytes.

--- benches/benches.rs
+++ benches/benches.rs
@@ -16,3 +16,15 @@ fn next_u32(b: &mut Bencher) {
         rng.next_u32()
     })
 }
+
+#[bench]
+fn fill_bytes(b: &mut Bencher) {
+    b.bytes = 1024*1024;
+    let mut rng = PcgRng::new_unseeded();
+
+    let mut x = vec![0; b.bytes as usize];
+
+    b.iter(|| {
+        rng.fill_bytes(x.as_mut_slice())
+    })
+}
