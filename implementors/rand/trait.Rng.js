00a034268d85228f3f04b920318534fb6cca0174
Coda Hale <coda.hale@gmail.com>
2015-04-17 13:58:01 +0000

    rebuild pages at 0b65689

--- implementors/rand/trait.Rng.js
+++ implementors/rand/trait.Rng.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['pcg'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/rand/rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='pcg/struct.PcgRng.html' title='pcg::PcgRng'>PcgRng</a>",];
+implementors['pcg'] = ["impl <a class='trait' href='http://doc.rust-lang.org/rand/rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='pcg/struct.PcgRng.html' title='pcg::PcgRng'>PcgRng</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/rand/trait.SeedableRng.js
+++ implementors/rand/trait.SeedableRng.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['pcg'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/rand/rand/trait.SeedableRng.html' title='rand::SeedableRng'>SeedableRng</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.array.html'>[<a href='http://doc.rust-lang.org/nightly/std/primitive.u64.html'>u64</a>; 2]</a>&gt; for <a class='struct' href='pcg/struct.PcgRng.html' title='pcg::PcgRng'>PcgRng</a>",];
+implementors['pcg'] = ["impl <a class='trait' href='http://doc.rust-lang.org/rand/rand/trait.SeedableRng.html' title='rand::SeedableRng'>SeedableRng</a>&lt;<a href='http://doc.rust-lang.org/nightly/std/primitive.array.html'>[<a href='http://doc.rust-lang.org/nightly/std/primitive.u64.html'>u64</a>; 2]</a>&gt; for <a class='struct' href='pcg/struct.PcgRng.html' title='pcg::PcgRng'>PcgRng</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
