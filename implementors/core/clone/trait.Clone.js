9bddfe92b6b60761297bb24a68c97fe6abb6f137
Coda Hale <coda.hale@gmail.com>
2016-04-28 18:47:30 +0000

    rebuild pages at 74448b8

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['pcg'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='pcg/struct.PcgRng.html' title='pcg::PcgRng'>PcgRng</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
